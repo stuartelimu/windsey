9c34093ac1e54f332b8115b2d8e5e504c145b072
ElimuStuart <stuartelimu@gmail.com>
2019-10-14 19:04:31 +0300

    add support section to dashboard section

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,15 +12,23 @@
                             <ul class="nav" id="myTab" role="tablist">
                                 <li class="nav-item">
                                     <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab"
-                                        aria-controls="home" aria-selected="true">Industrial construction</a>
+                                        aria-controls="home" aria-selected="true">FaaS Platform Standard</a>
                                 </li>
                                 <li class="nav-item">
                                     <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab"
-                                        aria-controls="profile" aria-selected="false">Mechanical engineering</a>
+                                        aria-controls="profile" aria-selected="false">Change Plan</a>
                                 </li>
                                 <li class="nav-item">
                                     <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab"
-                                        aria-controls="contact" aria-selected="false">Bridge construction</a>
+                                        aria-controls="contact" aria-selected="false">Profile</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" id="billing-tab" data-toggle="tab" href="#billing" role="tab"
+                                        aria-controls="billing" aria-selected="false">Billing</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" id="support-tab" data-toggle="tab" href="#support" role="tab"
+                                        aria-controls="support" aria-selected="false">Support</a>
                                 </li>
                             </ul>
                         </nav>
@@ -102,6 +110,50 @@
                                             sixth heaven winged lights bearing evening likeness. Above man thing. Fourth
                                             lights. That had you're stars. You subdue form days years likeness female.
                                         </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="tab-pane fade" id="billing" role="tabpanel" aria-labelledby="billing-tab">
+                                <div class="details-wrap">
+                                    <div class="details-info">
+                                        <h3>Payment methods</h3>
+                                        <p>Please enter your preferred payment method below. You can use a credit / debit card. 
+                                        </p>
+                                        
+                                    </div>
+                                    <div class="details-info">
+                                        <h3>Billing details</h3>
+                                        <p>Two midst that won't place waters likeness. Them place good. Darkness meat
+                                            moved creeping whales firmament light so were from and given saying light
+                                            was his fruitful two. Creature saying in was heaven appear in dominion can't
+                                            sixth heaven winged lights bearing evening likeness. Above man thing. Fourth
+                                            lights. That had you're stars. You subdue form days years likeness female.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="tab-pane fade" id="support" role="tabpanel" aria-labelledby="support-tab">
+                                <div class="details-wrap">
+                                    
+                                    <div class="details-info">
+                                        <h3>Contact Support</h3>
+                                        <p>Have a question? Send us a note and someone from the Windsey & Associates team will be in touch soon.
+                                        </p>
+                                    </div>
+                                    <div class="details-info">
+                                        <form class="form-contact contact_form" action="contact_process.php" method="post" id="contactForm" novalidate="novalidate">
+                                            <div class="row">
+                                                <div class="col-12">
+                                                    <div class="form-group">
+                                                        <textarea class="form-control w-100" name="message" id="message" cols="30" rows="9" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Message'" placeholder=" Enter Message"></textarea>
+                                                    </div>
+                                                </div>
+                                                
+                                            </div>
+                                            <div class="form-group mt-3">
+                                                <button type="submit" class="button button-contactForm boxed-btn">Send</button>
+                                            </div>
+                                        </form>
                                     </div>
                                 </div>
                             </div>
